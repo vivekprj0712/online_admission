df10cd9b516331f300d823659a372ba41b2f2f52
vivekprj0712 <prajapativivek949@gmail.com>
2024-02-07 22:42:45 +0530

    add new enrollment and pom.xml

--- src/main/java/com/arth/controller/EnrollmentController.java
+++ src/main/java/com/arth/controller/EnrollmentController.java
@@ -1,12 +1,14 @@
 package com.arth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.arth.Entity.EnrollmentEntity;
 import com.arth.repository.EnrollmentRepository;
 
+@Controller
 public class EnrollmentController {
 	
 	@Autowired
